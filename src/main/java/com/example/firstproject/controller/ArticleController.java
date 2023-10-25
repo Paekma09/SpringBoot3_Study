0c3785c5347f3f4f66e118fd694c5209c62536ae
KJ Nam <paekma09@gmail.com>
2023-10-26 01:21:09 +0900

    5_Chapter

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,10 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -50,5 +52,18 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         //3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        List<Article> articleEntityAll = articleRepository.findAll();
+        //ArrayList<Article> articleEntityAll = articleRepository.findAll();
+        //List<Article> articleEntityAll = (List<Article>) articleRepository.findAll();
+        //Iterable<Article> articleEntityAll = articleRepository.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityAll);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
