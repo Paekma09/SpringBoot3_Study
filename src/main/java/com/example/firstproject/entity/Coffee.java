c93b0ff7e779a3a194e5fa85dae8d9a121f4e33c
KJ Nam <paekma09@gmail.com>
2023-11-02 00:59:48 +0900

    11_Chapter_Exercise

---
+++ src/main/java/com/example/firstproject/entity/Coffee.java
@@ -0,0 +1,32 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Entity
+public class Coffee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column
+    String name;
+    @Column
+    String price;
+
+    public void patch(Coffee coffee) {
+        if (coffee.name != null) {
+            this.name = coffee.name;
+        }
+        if (coffee.price != null) {
+            this.price = coffee.price;
+        }
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ INSERT INTO article(title, content) VALUES ('다다다다', '3333');
 INSERT INTO member(email, password) VALUES ('aaaa@test.com', '1111');
 INSERT INTO member(email, password) VALUES ('bbbb@test.com', '2222');
 INSERT INTO member(email, password) VALUES ('cccc@test.com', '3333');
+
+INSERT INTO coffee(name, price) VALUES ('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES ('라떼', '5000');
+INSERT INTO coffee(name, price) VALUES ('카레 모카', '5500');
